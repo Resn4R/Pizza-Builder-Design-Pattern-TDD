30032d944ccce3807397eb9b4f173f26c0da2192
Resn4R <purview-joust.0x@icloud.com>
2023-11-02 18:52:19 +0000

    Initial Commit

---
+++ Pizza Builder TDD/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pizza Builder TDD
+//
+//  Created by Vito Borghi on 02/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pizza Builder TDD/Pizza_Builder_TDDApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pizza_Builder_TDDApp.swift
+//  Pizza Builder TDD
+//
+//  Created by Vito Borghi on 02/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct Pizza_Builder_TDDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
